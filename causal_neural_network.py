e6f54516fe1d0d43bf1b6dbd10d06ba8d177df27
Gilian <gilianponte@live.nl>
2023-08-18 13:39:53 +0200

    Update causal_neural_network.py

--- causal_neural_network.py
+++ causal_neural_network.py
@@ -86,12 +86,12 @@ def causal_neural_network(X, Y, T, scaling = True, simulations = 1, batch_size =
       print("the optimal architecture is: " + str(best_hps.values))
 
     cv = KFold(n_splits=folds) # K-fold validation
-    print("training model for m(x)")
+
     for k, (train_idx, test_idx) in enumerate(cv.split(X)):
       random.seed(k)
       np.random.seed(k)
       tf.random.set_seed(k)
-      print(f"Fold {k}:")
+      #print("training model for m(x)")
       model_m_x = tuner.hypermodel.build(best_hps)
       model_m_x.fit(
           X[train_idx],
@@ -105,22 +105,21 @@ def causal_neural_network(X, Y, T, scaling = True, simulations = 1, batch_size =
       model_m_x.build(input_shape = (None,X.shape[1]))
       model_m_x.load_weights(checkpoint_filepath_mx)
       m_x = model_m_x.predict(x=X[test_idx], verbose = 0).reshape(len(Y[test_idx])) # obtain \hat{m}(x) from test set
-      print("the mean of m_x = " + str(np.round(np.mean(m_x),2)) + ", std. = " + str(np.round(np.std(m_x),3)))
-
+      
       # obtain \tilde{Y} = Y_{i} - \hat{m}(x)
-      print("obtaining Y_tilde")
+      #print("obtaining Y_tilde")
       truth = Y[test_idx].T.reshape(len(Y[test_idx]))
       y_tilde = truth - m_x
       y_tilde_hat = np.concatenate((y_tilde_hat,y_tilde)) # cbind in r
 
       ## fit \hat{e}(x)
-      print("training model for e(x)")
+      #print("training model for e(x)")
       clf = LogisticRegression(verbose = 0).fit(X[train_idx], np.array(T[train_idx]).reshape(len(T[train_idx])))
       e_x = clf.predict_proba(X[test_idx]) # obtain \hat{e}(x)
-      print("the mean of e_x = " + str(np.round(np.mean(e_x[:,1]),2)) + ", std. = " + str(np.round(np.std(e_x[:,1]),3)))
+      print(f"Fold {k}: mean(m_x) = " + str(np.round(np.mean(m_x),2)) + ", sd(m_x) = " + str(np.round(np.std(m_x),3)) + " and mean(e_x) = " + str(np.round(np.mean(e_x[:,1]),2)) + ", sd(e_x) = " + str(np.round(np.std(e_x[:,1]),3)))
       
       # obtain \tilde{T} = T_{i} - \hat{e}(x)
-      print("obtaining T_tilde")
+      #print("obtaining T_tilde")
       truth = T[test_idx].T.reshape(len(T[test_idx]))
       T_tilde = truth - e_x[:,1]
       T_tilde_hat = np.concatenate((T_tilde_hat,T_tilde))
