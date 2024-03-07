0ea0cdaf41fba1f5817a9548e163d88c9953ea95
Gilian <gilianponte@live.nl>
2024-03-07 17:16:59 +0100

    Update strategy1.py

--- causal_inference/strategy1.py
+++ causal_inference/strategy1.py
@@ -215,7 +215,7 @@ def pcnn(X, Y, T, scaling=True, simulations=1, batch_size=100, epochs=100, max_e
     import pandas as pd
 
     # calculate epsilon
-    epsilon = np.round(tf_privacy.compute_dp_sgd_privacy(n=len(X), batch_size=batch_size, noise_multiplier=noise_multiplier, epochs=epochs, delta=1/len(X))[0], 2)
+    epsilon = np.round(tensorflow_privacy.compute_dp_sgd_privacy(n=len(X), batch_size=batch_size, noise_multiplier=noise_multiplier, epochs=epochs, delta=1/len(X))[0], 2)
     print("epsilon  = " +  str(epsilon) + ", the privacy risk increases with " + str(np.round((np.exp(epsilon)-1)*100, 2)) + " percent" )
 
     # callback settings for early stopping and saving
@@ -339,7 +339,7 @@ def pcnn(X, Y, T, scaling=True, simulations=1, batch_size=100, epochs=100, max_e
         print("training for tau hat")
         for fold, (train_idx, test_idx) in enumerate(cv.split(X)):
             tau_hat = tuner.hypermodel.build(best_hps)
-            tau_hat.compile(optimizer=tf_privacy.DPKerasAdamOptimizer(l2_norm_clip=4, noise_multiplier=noise_multiplier, num_microbatches=batch_size, learning_rate=0.001),
+            tau_hat.compile(optimizer=tensorflow_privacy.DPKerasAdamOptimizer(l2_norm_clip=4, noise_multiplier=noise_multiplier, num_microbatches=batch_size, learning_rate=0.001),
                             loss=tf.keras.losses.MeanSquaredError(reduction=tf.losses.Reduction.NONE), metrics=[ATE])
             history_tau = tau_hat.fit(
                 X[train_idx],
