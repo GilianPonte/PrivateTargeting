a5e4f319ae92fe52577d4a5456d8e9b8ac72d1dc
Gilian <gilianponte@live.nl>
2024-03-07 16:45:30 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+# Read the contents of README.md file for long description
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# Specify dependencies
+requirements = [
+    "scikit-learn",
+    "keras",
+    "tensorflow",
+    "tensorflow-privacy",
+    "keras-tuner"
+]
+
+# Define package metadata
+setuptools.setup(
+    name="causal_inference_package",
+    version="0.1",
+    author="Your Name",
+    author_email="your_email@example.com",
+    description="A package for causal inference methods",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/causal_inference_package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=requirements,
+)
