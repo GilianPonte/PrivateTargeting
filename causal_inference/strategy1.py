8ce2b5e9471b24f2966ae942780ff7f67367655a
Gilian <gilianponte@live.nl>
2024-04-23 11:34:29 +0200

    Update strategy1.py

--- causal_inference/strategy1.py
+++ causal_inference/strategy1.py
@@ -314,8 +314,8 @@ def pcnn(X, Y, T, scaling=True, simulations=1, batch_size=100, epochs=100, max_e
     T = np.array(pd.DataFrame(T).reindex(idx))
 
     # save models
-    checkpoint_filepath_mx = f'{directory}/m_x_{epsilon}.hdf5'
-    checkpoint_filepath_taux = f'{directory}/tau_x_{epsilon}.hdf5'
+    checkpoint_filepath_mx = f'{directory}/m_x.hdf5'
+    checkpoint_filepath_taux = f'{directory}/tau_x.hdf5'
     mx_callbacks = [callback,
       tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_filepath_mx, save_weights_only=False, monitor='val_loss', mode='min', save_freq="epoch", save_best_only=True),]
     tau_hat_callbacks = [callback,
@@ -400,4 +400,4 @@ def pcnn(X, Y, T, scaling=True, simulations=1, batch_size=100, epochs=100, max_e
         average_treatment_effect = np.mean(CATE_estimates)
         print(f"ATE = {np.round(np.mean(average_treatment_effect), 4)}, std(ATE) = {np.round(np.std(average_treatment_effect), 3)}")
         
-    return average_treatment_effect,all_CATE_estimates, epsilon
+    return average_treatment_effect,all_CATE_estimates
