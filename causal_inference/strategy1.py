75e5bf41c8e99a024728476728faa63070bd572b
Gilian <gilianponte@live.nl>
2024-04-29 16:16:30 +0200

    Update strategy1.py

--- causal_inference/strategy1.py
+++ causal_inference/strategy1.py
@@ -324,9 +324,9 @@ def pcnn(X, Y, T, scaling=True, batch_size=100, epochs=100, max_epochs=1, direct
     checkpoint_filepath_taux = f"{directory}/_{epsilon}_tau_x"
 
     mx_callbacks = [callback,
-      tf.keras.callbacks.ModelCheckpoint(filepath=str(directory+checkpoint_filepath_mx), save_weights_only=False, monitor='val_loss', mode='min', save_freq="epoch", save_best_only=True, save_format='tf'),]
+      tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_filepath_mx, save_weights_only=False, monitor='val_loss', mode='min', save_freq="epoch", save_best_only=True, save_format='tf'),]
     tau_hat_callbacks = [callback,
-      tf.keras.callbacks.ModelCheckpoint(filepath=str(directory+checkpoint_filepath_taux), save_weights_only=False, monitor='val_loss', mode='min', save_freq="epoch", save_best_only=True, save_format='tf'),]
+      tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_filepath_taux, save_weights_only=False, monitor='val_loss', mode='min', save_freq="epoch", save_best_only=True, save_format='tf'),]
     y_tilde_hat = []  # collect all the \tilde{Y}
     T_tilde_hat = []  # collect all the \tilde{T}
     m_x_hat = []  # collect all m_x_hat for print
