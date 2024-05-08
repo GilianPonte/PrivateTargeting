c1c4b52883f955666ff8dcd3028fe8e8081fda6c
Gilian <gilianponte@live.nl>
2024-05-08 15:34:36 +0200

    Update simulation.py

--- causal_inference/simulation.py
+++ causal_inference/simulation.py
@@ -12,10 +12,14 @@ from causal_inference import strategy1
 import time
 import secrets
 
-# read generate and write seeds function
-def generate_and_write_seeds(file_path, num_seeds):
-    # Generate random seeds
-    seeds = [secrets.randbits(32) for _ in range(num_seeds)]
+
+def generate_and_write_seeds(file_path, num_seeds, seed=None):
+    # If a seed is provided, use it for reproducibility
+    if seed is not None:
+        random.seed(seed)
+    
+    # Generate random seeds deterministically
+    seeds = [random.getrandbits(32) for _ in range(num_seeds)]
 
     # Write seeds to a text file
     with open(file_path, "w") as file:
@@ -24,26 +28,28 @@ def generate_and_write_seeds(file_path, num_seeds):
 
     print("Seeds have been written to", file_path)
 
-# read seeds function
 def read_file(file_path):
     with open(file_path, "r") as file:
         return [int(seed.strip()) for seed in file.readlines()]
+
+# Set the initial seed for reproducibility
+initial_seed = 422312
+
+# Generate and write seeds for seeds_data.txt
+generate_and_write_seeds("seeds_data.txt", 100, seed=initial_seed)
+generate_and_write_seeds("seeds_training.txt", 700, seed=initial_seed)
 
 # set time
 start_time = time.time()
 tensorflow.config.experimental.enable_op_determinism()
 os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
 
-# Generate and write seeds for seeds_data.txt
-generate_and_write_seeds("seeds_data.txt", 100)
-generate_and_write_seeds("seeds_training.txt", 700)
-
 # Read seeds_data and seeds_training from file
 seeds_data = read_file("seeds_data.txt")
 seeds_training = read_file("seeds_training.txt")
 
 # simulation parameters
-iterations = 100
+iterations = 1
 results_list = []
 noise_multipliers = [0,8.7,3.4,1.12,0.845,0.567,0.3543] # Initialize lists to store results for each noise multiplier
 
@@ -65,7 +71,7 @@ for i in range(iterations):
   for noise_index, noise_multiplier in enumerate(noise_multipliers):
     print(noise_multiplier)
 
-    for a in range(100):
+    for a in range(1):
       combined_number = (noise_index * 100) + a
       print("Combined number: {}".format(combined_number, combined_number))
       random.seed(seeds_training[combined_number])
@@ -118,7 +124,9 @@ for i in range(iterations):
           'true CATE': data[['tau']],
           'sample size': n,
           'data set' : i,
-          'iteration' : a
+          'iteration' : a,
+          'covariates' : x,
+
           })
 
       # Print or use the DataFrames as needed
